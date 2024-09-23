3c2b1ccd75756a4cc42bfa38519609fea527d1d8
Jan Sebastian Siwy <github@jansiwy.de>
2024-09-23 11:06:03 +0200

    Unify structure of module variables

--- variables.tf
+++ variables.tf
@@ -1,15 +1,24 @@
-variable "primary_domain_name" {
-  description = "A domain name for which the certificate should be issued"
-  type        = string
+variable "domain_names_to_zone_ids" {
+  type = map(string)
+
+  description = <<EOS
+Map of domain names (incl. `var.primary_domain_name`) to Route53 hosted zone IDs.
+EOS
 }
 
-variable "domain_names_to_zone_ids" {
-  description = "Map of domain names (incl. `var.primary_domain_name`) to Route53 hosted zone IDs"
-  type        = map(string)
+variable "primary_domain_name" {
+  type = string
+
+  description = <<EOS
+A domain name for which the certificate should be issued.
+EOS
 }
 
 variable "tags" {
-  description = "A mapping of tags to assign to the resource"
-  type        = map(string)
-  default     = {}
+  type    = map(string)
+  default = {}
+
+  description = <<EOS
+Map of tags assigned to all AWS resources created by this module.
+EOS
 }
